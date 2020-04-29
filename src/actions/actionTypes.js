f8336e8f68dff5025fdceb1ba48c0609a5fbb732
carlosribas <caduribas@gmail.com>
2020-04-29 11:32:36 +0100

    Remove exact match entries

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -30,7 +30,4 @@ export const EXAMPLE_SEQUENCE = 'EXAMPLE_SEQUENCE';
 export const CLEAR_SEQUENCE = 'CLEAR_SEQUENCE';
 export const FILE_UPLOAD = 'FILE_UPLOAD';
 
-// check exact match
-export const EXACT_MATCH = 'EXACT_MATCH';
-
 export const FETCH_RNACENTRAL_DATABASES = 'FETCH_RNACENTRAL_DATABASES';
